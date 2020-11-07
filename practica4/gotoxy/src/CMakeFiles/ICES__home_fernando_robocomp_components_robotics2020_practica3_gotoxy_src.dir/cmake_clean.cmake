bfc05ae7c2af16764abf3b18f152ce7533a63cd9
Fernando Díaz Fortuna <fdiazfor@alumnos.unex.es>
2020-11-07 17:49:12 +0100

    practica4

---
+++ practica4/gotoxy/src/CMakeFiles/ICES__home_fernando_robocomp_components_robotics2020_practica3_gotoxy_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_fernando_robocomp_components_robotics2020_practica3_gotoxy_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
