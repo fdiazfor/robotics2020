badf69308058dc10e46b7cd26b28e025c6095af1
Fernando Díaz Fortuna <fdiazfor@alumnos.unex.es>
2020-11-18 13:43:03 +0100

    creado el nuevo componente

---
+++ practica4/gotoxy2/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+#define USE_QTGUI
+
+
+#define PROGRAM_NAME    "gotoxy2"
+#define SERVER_FULL_NAME   "RoboComp gotoxy2:: gotoxy2"
+
+#endif
